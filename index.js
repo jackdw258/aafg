2ccf0d436f3c41cc8185736e0a503ed7aa476d8c
jackdw258 <64153468+jackdw258@users.noreply.github.com>
2025-01-03 10:22:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.MessageReactions,
+        GatewayIntentBits.DirectMessages
     ],
 });
 
